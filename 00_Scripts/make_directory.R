7e4de5186efd22802bdb337c5624b709b6cd4b9a
Ludek Stehlik <ludek.stehlik@gmail.com>
2018-07-06 07:57:45 +0200

    ...

--- 00_Scripts/make_directory.R
+++ 00_Scripts/make_directory.R
@@ -7,6 +7,7 @@ make_project_dir <- function(){
   dir_names <- c(
     "00_Data",
     "00_Scripts",
+    "00_Misc",
     "01_Business_Understanding",
     "02_Data_Understanding",
     "03_Data_Preparation",
@@ -14,8 +15,10 @@ make_project_dir <- function(){
     "05_Evaluation",
     "06_Deployment")
   
+  # Create directories
   dir_create(dir_names)
   
+  # List directories
   dir_ls()
   
 }
